677fc4f9ca8f1bd5c1ff28e1e5d4c91341460b7d
Kat <imhelvetika@gmail.com>
2025-09-18 09:13:57 -0500

    Initial commit!

---
+++ DisplayColorTester.py
@@ -0,0 +1,210 @@
+import tkinter as tk
+
+# MAIN WINDOW
+window = tk.Tk()
+window.title("Display Color Tester!")
+window.geometry("420x420")
+window.resizable(True, True)
+window.config(background="#323232")
+
+
+#     Windows
+
+
+# Black Window
+def set_black():
+    black_window = tk.Toplevel(window)
+    black_window.title("The Color Black!")
+    black_window.geometry("1280x720")
+    black_window.resizable(True, True)
+    black_window.configure(background="#000000")
+    return
+
+
+# Grey Window
+def set_grey():
+    grey_window = tk.Toplevel(window)
+    grey_window.title("The Color Grey!")
+    grey_window.geometry("1280x720")
+    grey_window.resizable(True, True)
+    grey_window.configure(background="#7F7F7F")
+
+
+# White Window
+def set_white():
+    white_window = tk.Toplevel(window)
+    white_window.title("The Color White!")
+    white_window.geometry("1280x720")
+    white_window.resizable(True, True)
+    white_window.configure(background="#FFFFFF")
+    return
+
+
+# Red Window
+def set_red():
+    red_window = tk.Toplevel(window)
+    red_window.title("The Color Red!")
+    red_window.geometry("1280x720")
+    red_window.resizable(True, True)
+    red_window.configure(background="#FF0000")
+    return
+
+
+# Green Window
+def set_green():
+    green_window = tk.Toplevel(window)
+    green_window.title("The Color Green!")
+    green_window.geometry("1280x720")
+    green_window.resizable(True, True)
+    green_window.configure(background="#00FF00")
+    return
+
+
+# Blue Window
+def set_blue():
+    blue_window = tk.Toplevel(window)
+    blue_window.title("The Color Blue!")
+    blue_window.geometry("1280x720")
+    blue_window.resizable(True, True)
+    blue_window.configure(background="#0000FF")
+    return
+
+
+#     Buttons
+
+
+# Close
+close_button = tk.Button(
+    window,
+    text="Close",
+    command=window.destroy,
+    font=("Helvetica", 10, "bold")
+)
+close_button.config(
+    activebackground="#FF0000",
+    activeforeground="#7F7F7F",
+    background="#7F7F7F",
+    foreground="#FF0000"
+)
+close_button.place(
+    relx=0.99,
+    rely=0.99,
+    anchor="se"
+)
+
+# Black Window Button
+black_window_button = tk.Button(
+    window,
+    text="Black",
+    command=set_black,
+    font=("Helvetica", 20, "bold")
+)
+black_window_button.config(
+    activebackground="#000000",
+    activeforeground="#323232",
+    background="#000000",
+    foreground="#323232"
+)
+black_window_button.place(
+    relx=0.01,
+    rely=0.01,
+    anchor="nw"
+)
+
+# Grey Window Button
+grey_window_button = tk.Button(
+    window,
+    text="Grey",
+    command=set_grey,
+    font=("Helvetica", 20, "bold")
+)
+grey_window_button.config(
+    activebackground="#7F7F7F",
+    activeforeground="#323232",
+    background="#7F7F7F",
+    foreground="#323232"
+)
+grey_window_button.place(
+    relx=0.5,
+    rely=0.01,
+    anchor="n"
+)
+
+# White Window Button
+white_window_button = tk.Button(
+    window,
+    text="White",
+    command=set_white,
+    font=("Helvetica", 20, "bold")
+)
+white_window_button.config(
+    activebackground="#FFFFFF",
+    activeforeground="#323232",
+    background="#FFFFFF",
+    foreground="#323232"
+)
+white_window_button.place(
+    relx=0.99,
+    rely=0.01,
+    anchor="ne"
+)
+
+# Red Window Button
+red_window_button = tk.Button(
+    window,
+    text="Red",
+    command=set_red,
+    font=("Helvetica", 20, "bold")
+)
+red_window_button.config(
+    activebackground="#FF0000",
+    activeforeground="#323232",
+    background="#FF0000",
+    foreground="#323232"
+)
+red_window_button.place(
+    relx=0.01,
+    rely=0.5,
+    anchor="w"
+)
+
+# Green Window Button
+green_window_button = tk.Button(
+    window,
+    text="Green",
+    command=set_green,
+    font=("Helvetica", 20, "bold")
+)
+green_window_button.config(
+    activebackground="#00FF00",
+    activeforeground="#323232",
+    background="#00FF00",
+    foreground="#323232"
+)
+green_window_button.place(
+    relx=0.49,
+    rely=0.5,
+    anchor="center"
+)
+
+# Blue Window Button
+blue_window_button = tk.Button(
+    window,
+    text="Blue",
+    command=set_blue,
+    font=("Helvetica", 20, "bold")
+)
+blue_window_button.config(
+    activebackground="#0000FF",
+    activeforeground="#323232",
+    background="#0000FF",
+    foreground="#323232"
+)
+blue_window_button.place(
+    relx=0.99,
+    rely=0.5,
+    anchor="e"
+)
+
+# START PROGRAM
+window.mainloop()
---
+++ README.md
@@ -0,0 +1,70 @@
+# | Display Color Tester!
+'Display Color Tester' is a
+very simple program written in
+Python, which will allow the
+user to choose a color
+to display in it's own
+resizable window!
+
+## | General Purpose of Creation:
+This tool aims to provide
+the user a simple way
+of troubleshooting possible display issues.
+Or even simply checking how
+colors look on a display!
+
+This ability to quickly display
+a single color on my
+own monitors, has personally helped
+me troubleshoot and identify display
+issues quite a few times,
+and aims to provide this
+same helpful experience to others!
+
+### | A Few Use Cases:
+- Using a plain white screen
+to identify dust or dirt
+on a display for easier
+cleaning!
+
+- Using a plain black screen
+to identify and correct how
+darker shades look on a
+display!
+
+- Using an RGB color to
+identify dead pixels, or dead
+sub-pixels in a display!
+
+# | Previews:
+The UI of this program
+is very simple, utilizing the
+native Python library, [***"tkinter"***](https://en.wikipedia.org/wiki/Tkinter)!
+
+## | The Main Menu:
+![MainMenu.png](images/MainMenu.png)
+
+## | The Colored Windows:
+![ColoredWindows.png](images/ColoredWindows.png)
+
+---
+
+> [!NOTE]
+> This program is by no
+means meant to improve upon,
+or be better than other
+programs that hold the same
+purpose or otherwise, provide the
+same function! It is simply
+a small project I've decided
+to create and maintain for
+fun! There are many alternatives
+which do not require a
+download, and are likely more
+feature rich. Such as web
+based options!
+> ## | A Few Examples:
+> - [***"Colour Test Screen"***](https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiK89X7tuKPAxUsJ0QIHccwMLEQFnoECCAQAQ&url=https%3A%2F%2Fwww.ledr.com%2Fcolours%2Fmulti.htm&usg=AOvVaw3x5-ypxllj9OhmxfWccYIZ&opi=89978449)
+> - [***"Test My Screen"***](https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiK89X7tuKPAxUsJ0QIHccwMLEQFnoECBkQAQ&url=https%3A%2F%2Ftestmyscreen.com%2F&usg=AOvVaw1DO2ZbntndujLjrXWsFPZ3&opi=89978449)
+> - [***"EIZO Monitor Test"***](https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiK89X7tuKPAxUsJ0QIHccwMLEQFnoECCEQAQ&url=https%3A%2F%2Fwww.eizo.be%2Fmonitor-test%2F&usg=AOvVaw16rOrGdhE8JgIxVfyaOGvT&opi=89978449)
+> - [***"Mobile Screen Test"***](https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&ved=2ahUKEwiK89X7tuKPAxUsJ0QIHccwMLEQFnoECBYQAQ&url=https%3A%2F%2Fmobilescreentest.com%2F&usg=AOvVaw1j6_Zi2BGH7tQbco9pWyeS&opi=89978449)
