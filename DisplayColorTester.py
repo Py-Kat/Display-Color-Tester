77300f7c589d311d4ef95ac98f62082f79ef4e7e
Kat <imhelvetika@gmail.com>
2025-09-18 22:18:24 -0500

    Moved functions for windows into seperate 'window_commands.py' file

--- DisplayColorTester.py
+++ DisplayColorTester.py
@@ -1,74 +1,13 @@
 import tkinter as tk
-
-# MAIN WINDOW
-window = tk.Tk()
-window.title("Display Color Tester!")
-window.geometry("420x420")
-window.resizable(True, True)
-window.config(background="#323232")
-
-
-#     Windows
-
-
-# Black Window
-def set_black():
-    black_window = tk.Toplevel(window)
-    black_window.title("The Color Black!")
-    black_window.geometry("1280x720")
-    black_window.resizable(True, True)
-    black_window.configure(background="#000000")
-    return
-
-
-# Grey Window
-def set_grey():
-    grey_window = tk.Toplevel(window)
-    grey_window.title("The Color Grey!")
-    grey_window.geometry("1280x720")
-    grey_window.resizable(True, True)
-    grey_window.configure(background="#7F7F7F")
-
-
-# White Window
-def set_white():
-    white_window = tk.Toplevel(window)
-    white_window.title("The Color White!")
-    white_window.geometry("1280x720")
-    white_window.resizable(True, True)
-    white_window.configure(background="#FFFFFF")
-    return
-
-
-# Red Window
-def set_red():
-    red_window = tk.Toplevel(window)
-    red_window.title("The Color Red!")
-    red_window.geometry("1280x720")
-    red_window.resizable(True, True)
-    red_window.configure(background="#FF0000")
-    return
-
-
-# Green Window
-def set_green():
-    green_window = tk.Toplevel(window)
-    green_window.title("The Color Green!")
-    green_window.geometry("1280x720")
-    green_window.resizable(True, True)
-    green_window.configure(background="#00FF00")
-    return
-
-
-# Blue Window
-def set_blue():
-    blue_window = tk.Toplevel(window)
-    blue_window.title("The Color Blue!")
-    blue_window.geometry("1280x720")
-    blue_window.resizable(True, True)
-    blue_window.configure(background="#0000FF")
-    return
-
+from window_commands import (
+    window,
+    set_black,
+    set_grey,
+    set_white,
+    set_red,
+    set_green,
+    set_blue
+)
 
 #     Buttons
 
